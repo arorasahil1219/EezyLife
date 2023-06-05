e238d5cce5f91c68ff515f08c142d2cfac01e2f1
sahil1219 <sahil_darora@hotmail.com>
2023-06-05 11:31:19 +0530

    job timing chnge

--- controller/order_controller.js
+++ controller/order_controller.js
@@ -309,10 +309,11 @@ async function putOrderDataInDb(data) {
   let arr = [];
   for (let item of data) {
     console.log('item.PurchaseDate original date:::::',item.PurchaseDate)
-    //let localDate = new Date(item.PurchaseDate);
+   // let localDate = new Date(item.PurchaseDate);
+    let localDate = item.PurchaseDate
     //console.log('localDate.toLocaleDateString():::',localDate.toLocaleDateString())
     //console.log('localDate.toLocaleTimeString():::',localDate.toLocaleTimeString())
-    let localDate =await refactorDate(item.PurchaseDate)
+    //let localDate =await refactorDate(item.PurchaseDate)
     //let utcDate =  item.PurchaseDate // '2023-02-02T05:59:26Z';
     // let date = new Date(utcDate);
     // let orderDate  = date.toLocaleString()
