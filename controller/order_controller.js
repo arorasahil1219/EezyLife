6771bee9e9154fbb5d0a9ea41075c1f68fdbc770
sahil1219 <sahil_darora@hotmail.com>
2023-09-16 08:42:40 +0530

    sync order

--- controller/order_controller.js
+++ controller/order_controller.js
@@ -146,7 +146,7 @@ const syncOrderJob = async () => {
         //console.log('syncing job now')
       let query = {
         MarketplaceIds: ["A21TJRUUN4KGV"],
-        CreatedAfter: "2023-01-04T00:00:00-07:00",
+        CreatedAfter: "2023-04-01T00:00:00-07:00",
       };
       let getAllCustomerDetails = await Customers.findAll({
         where: { isActive: 1 },
