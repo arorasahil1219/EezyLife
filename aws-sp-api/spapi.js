761a4441fc69a884546cc9b91086f9854aec3c72
sahil1219 <sahil_darora@hotmail.com>
2023-10-15 22:20:29 +0530

    seller aws

--- aws-sp-api/spapi.js
+++ aws-sp-api/spapi.js
@@ -6,6 +6,13 @@ async function execute_sp_api(operation, endpoint, path, query, refreshToken) {
     console.log("path",path)
     console.log("query",query)
     console.log("refreshToken",refreshToken)
+    console.log({
+      SELLING_PARTNER_APP_CLIENT_ID:process.env.SELLING_PARTNER_APP_CLIENT_ID,
+      SELLING_PARTNER_APP_CLIENT_SECRET:process.env.SELLING_PARTNER_APP_CLIENT_SECRET,
+      AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY:process.env.AWS_SECRET_ACCESS_KEY,
+      AWS_SELLING_PARTNER_ROLE:process.env.AWS_SELLING_PARTNER_ROLE          
+    })
     let sellingPartner = new SellingPartnerAPI({
       region: "eu",
       options: {
